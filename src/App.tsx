2a3f839124cf8c251588edb032f3c4f5ec770472
FlynnGeddes <flynn.geddes@cazoo.co.uk>
2021-12-14 16:54:48 +0000

    Added JSON Fetch - Displays all the cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,76 @@
-import React from 'react';
-import {Card, CardContent, CardActions, CardHeader, Typography, Avatar, CardMedia, Grid, Button} from '@mui/material';
-import data from './launch.json';
+import React, { useState, useEffect } from 'react';
+import {Card, CardContent, CardActions, CardHeader, Typography, Avatar, CardMedia, Grid, Button, CircularProgress} from '@mui/material';
+//import data from './launch.json';
 import './App.css';
 import { Box } from '@mui/system';
 
+
+interface Launch {
+  id: string
+  name: string
+  details: string
+  date: string
+  date_unix: number
+  links: {
+    patch: {
+      large: string
+    }
+    article: string
+    flickr?: any
+  }
+}
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [launches, setLaunches] = useState<Launch[]>([]);
+
+  useEffect(() => {
+    const url = "https://api.spacexdata.com/v4/launches";
+    const fetchData = async () => {
+      try{
+        const response = await fetch(url);
+        const json = await response.json()
+        setLaunches(json as any);
+        setLoading(false);
+      }catch(error){
+        console.log("error:", error)
+      }
+    }
+    fetchData();
+  }, []);
+    
+
+
+  if (loading) {
+    return (
+      <Box sx={{ height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <CircularProgress size={'10vh'} />
+      </Box>
+    )
+  }
+
     return (
       <div className="App">
+        <Box m={3} sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
         <Grid container justifyContent="center" spacing={4}> 
-        { data.map(launch => {
+        { launches.map(launch => {
           return(
               <Grid item xs={3}>
-              <LaunchCard id={launch.id} patch={launch.links.patch.large} title={launch.name} date={launch.date_unix} img={launch.links.flickr.original[1]} desc={launch.details} link={launch.links.article}/> 
-              </Grid>
-           
-          )})
-        }
-        
-        { data.map(launch => {
-          return(
-              <Grid item xs={3}>
-              <LaunchCard id={launch.id} patch={launch.links.patch.large} title={launch.name} date={launch.date_unix} img={launch.links.flickr.original[1]} desc={launch.details} link={launch.links.article}/> 
+              <LaunchCard
+                id={launch.id}
+                patch={launch.links.patch.large}
+                title={launch.name}
+                date={launch.date_unix}
+                img={launch.links.flickr.original[1]}
+                desc={launch.details}
+                link={launch.links.article}
+              /> 
               </Grid>
            
           )})
         }
         </Grid>
+        </Box>
       </div>
     );
   
@@ -42,7 +87,7 @@ interface LaunchCardProps {
 
 
 function LaunchCard(props: LaunchCardProps) {
-  const cardWidth = 345
+  const cardWidth = 400
   const milliseconds = props.date * 1000
   const dateObject = new Date(milliseconds)
   let launchDate = dateObject.toLocaleString("en-US", {month: "long", day: "numeric", year: "numeric"})
@@ -60,7 +105,7 @@ function LaunchCard(props: LaunchCardProps) {
         component="img"
         height={cardWidth/2}
         image={props.img}
-        alt="Rocket Picture"
+        alt="No Image Available"
       />
     <CardContent>
     <Typography paragraph variant="caption" color="text.secondary">Launch ID: {props.id}</Typography>
